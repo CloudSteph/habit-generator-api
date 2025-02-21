be0f06b0ab7699d2de44726e1d45d858ebd63e5f
Stephanie Liew <liewstephanie88@gmail.com>
2025-02-21 17:12:53 -0500

    Added logging for habit reset automation and ensure all routes are registered

--- main.py
+++ main.py
@@ -7,12 +7,23 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from database import SessionLocal
 import crud
 from routes import router
+import logging
+from datetime import datetime
+
+# Configure logging to write to a file
+logging.basicConfig(
+    filename="reset_log.txt",
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%M-%D %H:%M:%S"
+)
 
 # Initalize FastAPI app
-app = FastAPI(title="Randomized Habit Generator API",
-              description="An API to track and manage habits with random selection and automation.",
-              version="1.0.0"
-              )
+app = FastAPI(
+    title="Randomized Habit Generator API",
+    description="An API to track and manage habits with random selection and automation.",
+    version="1.0.0"
+)
 
 # Include habit routes from 'routes.py'
 app.include_router(router)
@@ -33,7 +44,7 @@ def scheduled_reset():
     finally: db.close()
 
 # Schedule the reset function to run daily at 12:00 AM
-scheduler.add_job(scheduled_reset, "cron", hour=0, minute=0)
+scheduler.add_job(scheduled_reset, "cron", minute="*")
 scheduler.start()
 
 @app.get("/")
